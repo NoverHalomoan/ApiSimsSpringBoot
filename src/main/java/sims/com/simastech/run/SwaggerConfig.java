4eefd73d86f2e57a62924d226c30cdb636c2b433
vernover <noverhalomoan98@gmail.com>
2025-02-04 19:07:53 +0700

    Deploy 4

--- src/main/java/sims/com/simastech/run/SwaggerConfig.java
+++ src/main/java/sims/com/simastech/run/SwaggerConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.models.info.Info;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -17,6 +19,19 @@ public class SwaggerConfig {
                         .title("API Service")
                         .version("1.0")
                         .description("Dokumentasi API dengan Swagger"));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
     }
 
 }
