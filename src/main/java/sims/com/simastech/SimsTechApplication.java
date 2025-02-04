8bd2e3615d67f76b7c0bb7bfc3207a33c6e1a44f
vernover <noverhalomoan98@gmail.com>
2025-02-04 17:34:22 +0700

    App Deploy2

--- src/main/java/sims/com/simastech/SimsTechApplication.java
+++ src/main/java/sims/com/simastech/SimsTechApplication.java
@@ -2,10 +2,12 @@ package sims.com.simastech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class SimsTechApplication {
 
+	@Profile({ "dev", "prod" })
 	public static void main(String[] args) {
 		SpringApplication.run(SimsTechApplication.class, args);
 	}
